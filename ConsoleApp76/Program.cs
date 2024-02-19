3a6f91689d9bb50f1a038adacffabf2ecce861f9
ygursu <yasemingrs@gmail.com>
2024-02-19 22:48:36 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp76/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedenyasigirilenkisininehliyetkontr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Klavyeden yaşı girilen kişinin ehliyet alıp alamayacağını gösteren C# programını yazınız.
+
+            Console.WriteLine("Lütfen Yaşınızı Giriniz");
+            int yas = Int32.Parse(Console.ReadLine());
+
+            if (yas >= 18)
+            {
+                //Doğruysa ehliyet alabilirsiniz  
+                Console.WriteLine("Ehliyet alabilirsiniz");
+            }
+            else
+            {
+                //Yanlışsa ehliyet alamazsınız
+                Console.WriteLine("Ehliyet alamazsınız");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
